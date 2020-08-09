f53aa222b29775034602ac5058382750ec1c8646
ouitresjolie <ninacontreras1@gmail.com>
2020-08-09 16:12:13 +0200

    add blog test

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import BlogLayout from '~/layouts/BlogLayout.vue'
 import '~/assets/scss/globals.scss'
 import Vuex from 'vuex'
 require('typeface-source-sans-pro')
@@ -11,6 +12,7 @@ export default function (Vue, { router, head, isClient, appOptions }) {
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.component('BlogLayout', BlogLayout)
 
   // Add attributes to HTML tag
   head.htmlAttrs = { lang: 'en' }
---
+++ src/pages/blogHTML/blogPostTwo.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout :sidebar="true">
+    <h1>Blog Post Two</h1>
+    <p>This is a test</p>
+  </Layout>
+</template>
+
+
+<script>
+import GitLink from '~/components/GitLink.vue'
+import Shortcut from '~/components/Shortcut.vue'
+
+export default {
+  components: {
+    GitLink,
+    Shortcut
+  },
+  data() {
+    return {
+      description: 'The ultimate static generated documentation theme for the JAM-stack'
+    }
+  },
+  metaInfo() {
+    return {
+      title: this.description,
+      meta: [
+        { key: 'description', name: 'description', content: 'A theme for static site documentation based on Gridsome, ready to deploy to Netlify in one click.' }
+      ]
+    }
+  }
+}
+</script>
